ea68236ef0f9be447f67b982c9d7e1e2b22069a5
Дмитрий <shcheklein.dev@gmail.com>
2023-05-25 01:05:10 +0300

    Add: async chunk

--- files/counter.js
+++ files/counter.js
@@ -1,14 +1,43 @@
 const container = document.getElementById("rootCounter");
 const root = ReactDOM.createRoot(container);
-const {configureStore,createSlice} = RTK;
+const {configureStore,createSlice,createAsyncThunk} = RTK;
 const {Provider, useSelector, useDispatch} = ReactRedux;
 
 const initialState = {
   value: 0,
-  data: null,
-  isLoading: false,
-  error: null,
+  users: [],
+  usersLoading: false,
+  usersError: false,
+  userAddLoading: false
 };
+const createUserAction = createAsyncThunk(
+  'data/createUser',
+  async (userData, { extra: api }) => {
+    await new Promise(resolve=>{
+      setTimeout(()=>{
+        resolve()
+      }, 1000)
+    })
+    const {data} = await api.post(`/users`, {userData});
+    const newUser = {id: data.id, ...data.userData};
+    console.log('new', newUser);
+    return newUser;
+  },
+);
+const fetchUsersAction = createAsyncThunk(
+  'data/fetchUsers',
+  async (_, { extra: api }) => {
+    await new Promise(resolve=>{
+      setTimeout(()=>{
+        resolve()
+      }, 1000)
+    })
+    const {data} = await api.get(`/users`);
+
+    return data;
+  },
+);
+
 const counterSlice = createSlice({
   name: 'counter',
   initialState,
@@ -19,25 +48,43 @@ const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
-    startLoading: (state) => {
-      state.isLoading = true;
-    },
-    hasError: (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
-    setData: (state, action) => {
-      state.isLoading = false;
-      state.data = action.payload;
-    }
   },
+  extraReducers(builder) {
+    builder.addCase(createUserAction.fulfilled, (state, action) => {
+      state.users = [...state.users, action.payload];
+      state.userAddLoading = false;
+    });
+    builder.addCase(createUserAction.pending, (state) => {
+      state.userAddLoading = true;
+    });
+    builder.addCase(fetchUsersAction.fulfilled, (state, action) => {
+      state.users = action.payload;
+      state.usersLoading = false;
+    });
+    builder.addCase(fetchUsersAction.pending, (state) => {
+      state.usersLoading = true;
+    });
+    builder.addCase(fetchUsersAction.rejected, (state) => {
+      state.usersLoading = false;
+    });
+  }  
 });
-const { increment, decrement,startLoading, hasError, setData } = counterSlice.actions;
+const { increment, decrement } = counterSlice.actions;
+
+const api = axios.create({
+  baseURL: 'https://jsonplaceholder.typicode.com',
+});
 
 const store = configureStore({
   reducer: {
     counter: counterSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    thunk: {
+      extraArgument: api,
+    },
+  }),
 });
 
 root.render(
@@ -61,37 +108,42 @@ function App() {
 
 function Counter() {
   const count = useSelector((state) => state.counter.value);
-  const data = useSelector((state) => state.data);
-  const isLoading = useSelector((state) => state.isLoading);
-  const d = useDispatch();
-  console.log(data);
-  const fetchData = () => async (dispatch) => {
-    dispatch(startLoading());
-    try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-      dispatch(setData(response.data));
-    } catch (error) {
-      dispatch(hasError(error.message));
-    }
-  };
+  const users = useSelector((state) => state.counter.users);
+  const isLoading = useSelector((state) => state.counter.usersLoading);
+  const userAddLoading = useSelector((state) => state.counter.userAddLoading);
+  const dispatch = useDispatch();
+  console.log(users);
 
-  const onClick = ()=>{
-    d(fetchData)
+  const onClick = ()=>{    
+    dispatch(fetchUsersAction())
+  }
+  const onAdd = ()=>{    
+    dispatch(createUserAction({
+      "name": "Bob",
+      "username": "Bobobobob",
+      "email": "Sincere@april.biz",
+    }))
   }
 
   return (
     <div>
       <h1>{count}</h1>
-      <button className="button" onClick={() => d(increment())}>+</button>
-      <button className="button" onClick={() => d(decrement())}>-</button>
+      <button className="button" onClick={() => dispatch(increment())}>+</button>
+      <button className="button" onClick={() => dispatch(decrement())}>-</button>
       <hr/>
-      {isLoading? (<>Загрузка</>):(
+      {isLoading? (<h1>Загрузка...</h1>):(
 
-        <pre>
-        {data}
-        </pre>
+        <ul>
+        {users.map(el=>(
+        <li key={el.id}>
+          <h2>{el.name}-{el.id}</h2>
+        </li>
+        ))}
+        </ul>
       )}
+      {userAddLoading && <>Добавляю юзера</>}
       <button className="button" onClick={onClick}>fetch</button>
+      <button className="button" onClick={onAdd}>addUser</button>
 
     </div>
   );
