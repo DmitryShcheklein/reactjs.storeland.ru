ea52e0028d6ba7080059c80180bd1fa7a3a0256c
Dmitry <shcheklein.dev@gmail.com>
2025-10-13 23:48:36 +0300

    feat: start new cart

--- src/js/cart-query-new.js
+++ src/js/cart-query-new.js
@@ -1,84 +1,37 @@
-// import React from 'https://esm.sh/react@18.2.0';
-// import ReactDOM from 'https://esm.sh/react-dom@18.2.0';
-// import {
-//   QueryClient,
-//   QueryClientProvider,
-// } from 'https://esm.sh/@tanstack/react-query';
-// // import { ReactQueryDevtools } from 'https://esm.sh/@tanstack/react-query-devtools';
-
-// const queryClient = new QueryClient();
-
-// const container = document.getElementById('root-cart');
-// const root = ReactDOM.createRoot(container);
-
-// root.render(
-//   <QueryClientProvider client={queryClient}>
-//     <App />
-//     {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-//   </QueryClientProvider>
-// );
-
-// function App() {
-//   return (
-//     <>
-//       <h1>hiiii</h1>
-//     </>
-//   );
-// }
-
-import React, { useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import {
-  QueryClient,
-  QueryClientProvider,
-  useQuery,
-} from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+const { createRoot } = ReactDOM;
+const { QueryClient, QueryClientProvider } = ReactQuery;
+const { ReactQueryDevtools } = window.ReactQueryDevtools;
+const { useQuickFormData, useCart } = window.ReactQueryHooks;
 
 const queryClient = new QueryClient();
 
-function App() {
-  const [count, setCount] = useState(0);
+function Cart() {
+  const { data } = useQuickFormData();
 
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['time', count],
-    queryFn: async () => {
-      const res = await fetch('https://worldtimeapi.org/api/ip');
-      return res.json();
-    },
+  const [firstDelivery] = data?.deliveries || [];
+  console.log(firstDelivery?.id);
+  const { data: cartData } = useCart({
+    deliveryId: firstDelivery?.id,
+    zoneId: firstDelivery?.zoneList[0]?.zoneId,
+    couponCode: '',
+    isCouponSend: false,
   });
-
-  if (isLoading) return <p>Загрузка...</p>;
-  if (isError) return <p>Ошибка при загрузке данных</p>;
-
+  console.log(cartData);
   return (
-    <div style={{ fontFamily: 'sans-serif', padding: '20px' }}>
-      <h1>React + TanStack Query</h1>
-      <p>
-        <b>Текущее время:</b> {data.datetime}
-      </p>
-      <button
-        onClick={() => setCount((c) => c + 1)}
-        style={{
-          padding: '8px 14px',
-          borderRadius: '8px',
-          border: '1px solid #ccc',
-          background: '#f5f5f5',
-          cursor: 'pointer',
-        }}
-      >
-        Обновить
-      </button>
-
-      {/* Панель Devtools */}
-      <ReactQueryDevtools initialIsOpen={true} />
+    <div>
+      <h2>hi</h2>
     </div>
   );
 }
 
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Cart />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+}
+
 const root = createRoot(document.getElementById('root'));
-root.render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
+root.render(<App />);
---
+++ src/js/react-query-hooks.js
@@ -0,0 +1,82 @@
+const { useQuery } = ReactQuery;
+
+const QUERY_KEYS = {
+  Cart: 'Cart',
+  QuickFormData: 'QuickFormData',
+};
+
+function useQuickFormData() {
+  return useQuery({
+    queryKey: [QUERY_KEYS.QuickFormData],
+    queryFn: async () => {
+      const { data } = await axios.get(`/cart/add`, {
+        responseType: 'text',
+        params: {
+          ajax_q: 1,
+          fast_order: 1,
+        },
+      });
+
+      return JSON.parse(data);
+    },
+  });
+}
+
+function useCart({ deliveryId, zoneId, couponCode, isCouponSend }) {
+  return useQuery({
+    queryKey: [QUERY_KEYS.Cart],
+    queryFn: async () => {
+      console.log(deliveryId);
+      const formData = new FormData();
+      formData.append('form[delivery][id]', deliveryId);
+      formData.append('form[delivery][zone_id]', zoneId);
+
+      if (isCouponSend) {
+        formData.append('form[coupon_code]', couponCode);
+      }
+
+      window.CART?.cartItems?.forEach((item) =>
+        formData.append(
+          `form[quantity][${item.GOODS_MOD_ID}]`,
+          item.ORDER_LINE_QUANTITY
+        )
+      );
+
+      const { data: cartPageDataString } = await axios.post(`/cart`, formData, {
+        responseType: 'text',
+        params: {
+          only_body: 1,
+          hash: window.HASH,
+        },
+      });
+      const cartPageData = JSON.parse(cartPageDataString);
+
+      let orderStepsPageData;
+      if (isCouponSend && couponCode) {
+        const { cartRelatedGoods } = cartPageData;
+        const { data: stepsOrderDataString } = await axios.post(
+          `/order/stage/confirm`,
+          formData,
+          {
+            responseType: 'text',
+            params: {
+              only_body: 1,
+              ajax_q: 1,
+            },
+          }
+        );
+        orderStepsPageData = JSON.parse(stepsOrderDataString);
+        orderStepsPageData.cartRelatedGoods = cartRelatedGoods; // BUG: в пошаговом заказе нет массива сопутствующих
+      }
+
+      return orderStepsPageData || cartPageData;
+    },
+
+    enabled: Boolean(deliveryId),
+  });
+}
+
+window.ReactQueryHooks = {
+  useQuickFormData,
+  useCart,
+};
