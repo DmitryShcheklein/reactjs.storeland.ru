aae0787af11ae3aaa6d9fdf61700182d60a320f2
Dmitry <shcheklein.dev@gmail.com>
2025-10-15 12:43:50 +0300

    fix: query structure

--- src/js/cart-query-new.js
+++ src/js/cart-query-new.js
@@ -1,185 +1,183 @@
 const { createRoot } = ReactDOM;
-const { QueryClient, QueryClientProvider } = ReactQuery;
+const { QueryClient, QueryClientProvider, useQuery, useMutation } = ReactQuery;
 const { ReactQueryDevtools } = window.ReactQueryDevtools;
-const {
-  useQuickFormData,
-  useCart,
-  useClearCartMutation,
-  useClearCartItemMutation,
-} = window.ReactQueryHooks;
-const { QUERY_KEYS } = window.QUERY_KEYS;
 
-const queryClient = new QueryClient();
+const { quickFormApi, cartApi } = window.ReactQueryHooks;
 
 function Cart() {
-  const { data } = useQuickFormData();
-  // console.log('quickFormdata', data);
-  const [firstDelivery] = data?.deliveries || [];
+  const { data: quickFormData } = useQuery(quickFormApi.getQuickFormData());
+  const [firstDelivery] = quickFormData?.deliveries || [];
+  const { data: cartData } = useQuery(
+    cartApi.getCart({
+      deliveryId: firstDelivery?.id,
+      zoneId: firstDelivery?.zoneList[0]?.zoneId,
+      couponCode: '0000',
+      isCouponSend: !!1,
+    })
+  );
 
-  const { data: cartData } = useCart({
-    // deliveryId: firstDelivery?.id,
-    // zoneId: firstDelivery?.zoneList[0]?.zoneId,
-    couponCode: '0000',
-    isCouponSend: false,
+  const clearCartMutation = useMutation({
+    mutationFn: cartApi.clearCart,
+    onSuccess: async () => {
+      queryClient.invalidateQueries({ queryKey: [cartApi.baseKey] });
+    },
   });
-  // console.log('cartData', cartData);
 
-  const clearCartMutation = useClearCartMutation();
-  const clearCartItemMutation = useClearCartItemMutation({
-    onSuccess: () => {
-      console.log('success clear cart item');
-      queryClient.invalidateQueries({ queryKey: [QUERY_KEYS.Cart] });
+  const clearCartItemMutation = useMutation({
+    mutationFn: cartApi.deleteItem,
+    onSuccess: async () => {
+      queryClient.invalidateQueries({ queryKey: [cartApi.baseKey] });
     },
   });
 
   return (
     <div className="container">
-      <section className="section">
-        <h1 className="title is-2">Корзина</h1>
+      <h1 className="title is-2">Корзина</h1>
 
-        {!cartData?.cartItems?.length ? (
-          <div className="notification is-warning">
-            <p className="is-size-5">Ваша корзина пуста</p>
-          </div>
-        ) : (
-          <>
-            <div className="box">
-              <table className="table is-fullwidth is-striped is-hoverable">
-                <thead>
-                  <tr>
-                    <th>Товар</th>
-                    <th>Цена</th>
-                    <th>Количество</th>
-                    <th>Сумма</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {cartData?.cartItems.map((goods) => (
-                    <tr key={goods.GOODS_ID}>
-                      <td>
-                        <div className="is-flex is-align-items-center">
-                          {goods.GOODS_IMAGE && (
-                            <figure className="image is-64x64 mr-2">
-                              <img
-                                src={goods.GOODS_IMAGE}
-                                alt={goods.GOODS_NAME}
-                              />
-                            </figure>
-                          )}
-                          <div>
-                            <p className="is-size-5">{goods.GOODS_NAME}</p>
-                            <p className="is-size-7 has-text-grey">
-                              {goods.GOODS_MOD_ART_NUMBER}
-                            </p>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="has-text-weight-bold">
-                        {goods.GOODS_MOD_PRICE_NOW}
-                      </td>
-                      <td>
-                        <div className="field has-addons">
-                          <p className="control">
-                            <button className="button is-small">-</button>
-                          </p>
-                          <p className="control">
-                            <input
-                              className="input is-small has-text-centered"
-                              type="text"
-                              value={goods.ORDER_LINE_QUANTITY}
-                              readOnly
-                              style={{ width: '50px' }}
+      {!cartData?.cartItems?.length ? (
+        <div className="notification is-warning">
+          <p className="is-size-5">Ваша корзина пуста</p>
+        </div>
+      ) : (
+        <>
+          <div className="box">
+            <table className="table is-fullwidth is-striped is-hoverable">
+              <thead>
+                <tr>
+                  <th>Товар</th>
+                  <th>Цена</th>
+                  <th>Количество</th>
+                  <th>Сумма</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartData?.cartItems.map((goods) => (
+                  <tr key={goods.GOODS_ID}>
+                    <td>
+                      <div className="is-flex is-align-items-center">
+                        {goods.GOODS_IMAGE && (
+                          <figure className="image is-64x64 mr-2">
+                            <img
+                              src={goods.GOODS_IMAGE}
+                              alt={goods.GOODS_NAME}
                             />
-                          </p>
-                          <p className="control">
-                            <button className="button is-small">+</button>
+                          </figure>
+                        )}
+                        <div>
+                          <p className="is-size-5">{goods.GOODS_NAME}</p>
+                          <p className="is-size-7 has-text-grey">
+                            {goods.GOODS_MOD_ART_NUMBER}
                           </p>
                         </div>
-                      </td>
-                      <td className="has-text-weight-bold">
-                        {goods.ORDER_LINE_PRICE_NOW}
-                      </td>
-                      <td>
-                        <button
-                          className="button is-small is-danger is-light"
-                          onClick={() =>
-                            clearCartItemMutation.mutate(goods.GOODS_MOD_ID)
-                          }
-                        >
-                          <span className="icon">
-                            <i className="icon-trash"></i>
-                          </span>
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                      </div>
+                    </td>
+                    <td className="has-text-weight-bold">
+                      {goods.GOODS_MOD_PRICE_NOW}
+                    </td>
+                    <td>
+                      <div className="field has-addons">
+                        <p className="control">
+                          <button className="button is-small">-</button>
+                        </p>
+                        <p className="control">
+                          <input
+                            className="input is-small has-text-centered"
+                            type="text"
+                            value={goods.ORDER_LINE_QUANTITY}
+                            readOnly
+                            style={{ width: '50px' }}
+                          />
+                        </p>
+                        <p className="control">
+                          <button className="button is-small">+</button>
+                        </p>
+                      </div>
+                    </td>
+                    <td className="has-text-weight-bold">
+                      {goods.ORDER_LINE_PRICE_NOW}
+                    </td>
+                    <td>
+                      <button
+                        className="button is-small is-danger is-light"
+                        onClick={() =>
+                          clearCartItemMutation.mutate(goods.GOODS_MOD_ID)
+                        }
+                      >
+                        <span className="icon">
+                          <i className="icon-trash"></i>
+                        </span>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
 
-            <div className="columns">
-              <div className="column is-8">
-                <div className="field">
-                  <div className="control">
-                    <input
-                      className="input"
-                      type="text"
-                      placeholder="Введите промокод"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="column is-4">
-                <div className="box">
-                  <div className="content">
-                    <p className="is-size-5">
-                      Итого:{' '}
-                      <span className="has-text-weight-bold">
-                        {cartData?.CART_SUM_NOW}
-                      </span>
-                    </p>
-                    <p className="is-size-5">
-                      Скидка:{' '}
-                      <span className="has-text-weight-bold has-text-danger">
-                        {cartData?.CART_SUM_DISCOUNT}
-                      </span>
-                    </p>
-                    <p className="is-size-5">
-                      Доставка:{' '}
-                      <span className="has-text-weight-bold">
-                        {cartData?.CART_SUM_DELIVERY}
-                      </span>
-                    </p>
-                    <hr />
-                    <p className="is-size-4">
-                      Итого с доставкой и скидкой:{' '}
-                      <span className="has-text-weight-bold">
-                        {cartData?.CART_SUM_NOW_WITH_DELIVERY_AND_DISCOUNT}
-                      </span>
-                    </p>
-                  </div>
-
-                  <div className="buttons is-flex is-justify-content-space-between">
-                    <button
-                      className="button is-danger"
-                      onClick={() => clearCartMutation.mutate()}
-                    >
-                      Очистить корзину
-                    </button>
-                    <button className="button is-primary">
-                      Оформить заказ
-                    </button>
-                  </div>
+          <div className="columns">
+            <div className="column is-8">
+              <div className="field">
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Введите промокод"
+                  />
                 </div>
               </div>
             </div>
-          </>
-        )}
-      </section>
+            <div className="column is-4">
+              <div className="box">
+                <div className="content">
+                  <p className="is-size-5">
+                    Итого:{' '}
+                    <span className="has-text-weight-bold">
+                      {cartData?.CART_SUM_NOW}
+                    </span>
+                  </p>
+                  <p className="is-size-5">
+                    Скидка:{' '}
+                    <span className="has-text-weight-bold has-text-danger">
+                      {cartData?.CART_SUM_DISCOUNT}
+                    </span>
+                  </p>
+                  <p className="is-size-5">
+                    Доставка:{' '}
+                    <span className="has-text-weight-bold">
+                      {cartData?.CART_SUM_DELIVERY}
+                    </span>
+                  </p>
+                  <hr />
+                  <p className="is-size-4">
+                    Итого с доставкой и скидкой:{' '}
+                    <span className="has-text-weight-bold">
+                      {cartData?.CART_SUM_NOW_WITH_DELIVERY_AND_DISCOUNT}
+                    </span>
+                  </p>
+                </div>
+
+                <div className="buttons is-flex is-justify-content-space-between">
+                  <button
+                    className="button is-danger"
+                    onClick={() => clearCartMutation.mutate()}
+                  >
+                    Очистить корзину
+                  </button>
+                  <button className="button is-primary">Оформить заказ</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
+
+const queryClient = new QueryClient();
+
+queryClient.prefetchQuery(quickFormApi.getQuickFormData());
 
 function App() {
   return (
--- src/js/react-query-hooks.js
+++ src/js/react-query-hooks.js
@@ -1,108 +1,101 @@
-const { useQuery, useMutation } = ReactQuery;
+const { useQuery, useMutation, queryOptions } = ReactQuery;
 
 const QUERY_KEYS = {
   Cart: 'Cart',
   QuickFormData: 'QuickFormData',
 };
 
-function useQuickFormData() {
-  return useQuery({
-    queryKey: [QUERY_KEYS.QuickFormData],
-    queryFn: async () => {
-      const { data } = await axios.get(`/cart/add`, {
-        responseType: 'text',
-        params: {
-          ajax_q: 1,
-          fast_order: 1,
-        },
-      });
+const quickFormApi = {
+  baseKey: QUERY_KEYS.QuickFormData,
+  getQuickFormData: () => {
+    return queryOptions({
+      queryKey: [QUERY_KEYS.QuickFormData],
+      queryFn: async () => {
+        const { data } = await axios.get(`/cart/add`, {
+          responseType: 'text',
+          params: {
+            ajax_q: 1,
+            fast_order: 1,
+          },
+        });
 
-      return JSON.parse(data);
-    },
-  });
-}
+        return JSON.parse(data);
+      },
+    });
+  },
+};
 
-function useCart({ deliveryId, zoneId, couponCode, isCouponSend }) {
-  return useQuery({
-    queryKey: [QUERY_KEYS.Cart],
-    queryFn: async () => {
-      const formData = new FormData();
+const cartApi = {
+  baseKey: QUERY_KEYS.Cart,
+  getCart: ({ deliveryId, zoneId, couponCode, isCouponSend } = {}) => {
+    return queryOptions({
+      queryKey: [QUERY_KEYS.Cart, deliveryId, zoneId],
+      initialData: window.CART,
+      queryFn: async () => {
+        const formData = new FormData();
 
-      if (deliveryId) {
-        formData.append('form[delivery][id]', deliveryId);
-      }
+        if (deliveryId) {
+          formData.append('form[delivery][id]', deliveryId);
+        }
 
-      if (zoneId) {
-        formData.append('form[delivery][zone_id]', zoneId);
-      }
+        if (zoneId) {
+          formData.append('form[delivery][zone_id]', zoneId);
+        }
 
-      if (isCouponSend) {
-        formData.append('form[coupon_code]', couponCode);
-      }
+        if (isCouponSend) {
+          formData.append('form[coupon_code]', couponCode);
+        }
 
-      window.CART?.cartItems?.forEach((item) =>
-        formData.append(
-          `form[quantity][${item.GOODS_MOD_ID}]`,
-          item.ORDER_LINE_QUANTITY
-        )
-      );
+        window.CART?.cartItems?.forEach((item) =>
+          formData.append(
+            `form[quantity][${item.GOODS_MOD_ID}]`,
+            item.ORDER_LINE_QUANTITY
+          )
+        );
 
-      const { data: cartPageDataString } = await axios.post(`/cart`, formData, {
-        responseType: 'text',
-        params: {
-          only_body: 1,
-          hash: window.HASH,
-        },
-      });
-      const cartPageData = JSON.parse(cartPageDataString);
-
-      let orderStepsPageData;
-      if (isCouponSend && couponCode) {
-        const { cartRelatedGoods } = cartPageData;
-        const { data: stepsOrderDataString } = await axios.post(
-          `/order/stage/confirm`,
+        const { data: cartPageDataString } = await axios.post(
+          `/cart`,
           formData,
           {
             responseType: 'text',
             params: {
               only_body: 1,
-              ajax_q: 1,
+              hash: window.HASH,
             },
           }
         );
-        orderStepsPageData = JSON.parse(stepsOrderDataString);
-        orderStepsPageData.cartRelatedGoods = cartRelatedGoods; // BUG: в пошаговом заказе нет массива сопутствующих
-      }
+        const cartPageData = JSON.parse(cartPageDataString);
 
-      return orderStepsPageData || cartPageData;
-    },
-    // enabled: Boolean(deliveryId),
-  });
-}
-function useClearCartMutation(options) {
-  return useMutation({
-    mutationFn: async () => {
-      await axios.get(`/cart/truncate/`);
+        let orderStepsPageData;
+        if (isCouponSend && couponCode) {
+          const { cartRelatedGoods } = cartPageData;
+          const { data: stepsOrderDataString } = await axios.post(
+            `/order/stage/confirm`,
+            formData,
+            {
+              responseType: 'text',
+              params: {
+                only_body: 1,
+                ajax_q: 1,
+              },
+            }
+          );
+          orderStepsPageData = JSON.parse(stepsOrderDataString);
+          orderStepsPageData.cartRelatedGoods = cartRelatedGoods; // BUG: в пошаговом заказе нет массива сопутствующих
+        }
 
-      queryClient.setQueryData([QUERY_KEYS.Cart], {});
-    },
-    ...options,
-  });
-}
+        return orderStepsPageData || cartPageData;
+      },
+      enabled: Boolean(deliveryId || zoneId),
+    });
+  },
+  clearCart: async () => await axios.get(`/cart/truncate/`),
+  deleteItem: async (itemId) => {
+    await axios.get(`/cart/delete/${itemId}`);
+  },
+};
 
-function useClearCartItemMutation(options) {
-  return useMutation({
-    mutationFn: async (itemId) => {
-      await axios.get(`/cart/delete/${itemId}`);
-    },
-    ...options,
-  });
-}
-
-window.QUERY_KEYS = QUERY_KEYS;
 window.ReactQueryHooks = {
-  useQuickFormData,
-  useCart,
-  useClearCartMutation,
-  useClearCartItemMutation,
+  quickFormApi,
+  cartApi,
 };
